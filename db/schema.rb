d2ed43c0215b6a7c0f40431665f8aac1b04345c8
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-08-15 16:52:11 +0200

    add grind options to product for custom choice in the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_131930) do
+ActiveRecord::Schema.define(version: 2020_08_15_142923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_131930) do
     t.datetime "updated_at", null: false
     t.integer "weight"
     t.integer "price_cents", default: 0, null: false
+    t.string "grind_options", default: [], array: true
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -87,6 +87,7 @@ Product.create!(
   name:'Cerro de Jesus farm',
   region: 'Jalapa, Neuva Segovia',
   process: 'washed',
+  grind_options: ['Whole Beans','Ground for filter','Ground for Aeropress'],
   cupping_notes: 'coffee cherry, lemon',
   description: 'Cerro de Jesus or Jesus Mountain Farm is located in Jalapa, Neuva Segovia, on the second highest peak of Nicaragua. The farm spreads through 700 hectares of which about 400 hectares are a beautiful forest reserve.
   In 2017, Jesus Mountain Farm decided to partner with Cafetos de Segovia, a small specialty mill and export company run by two sisters Martha and Ana Albir. The Albir sisters inherited from their father the passion and knowledge to grow their own coffee. Later they acquired a mill and by 2016 they were providing milling and export services to neighboring small coffee farms. This new venture allowed them to contact directly with farms with socially conscious practices and they were also able to focus in separating of coffee by variety.',
@@ -100,6 +101,7 @@ Product.create!(
   name:'Cerro de Jesus farm',
   region: 'Jalapa, Neuva Segovia',
   process: 'washed',
+  grind_options: ['Whole Beans','Ground for filter','Ground for Aeropress'],
   cupping_notes: 'coffee cherry, lemon',
   description: 'Cerro de Jesus or Jesus Mountain Farm is located in Jalapa, Neuva Segovia, on the second highest peak of Nicaragua. The farm spreads through 700 hectares of which about 400 hectares are a beautiful forest reserve.
   In 2017, Jesus Mountain Farm decided to partner with Cafetos de Segovia, a small specialty mill and export company run by two sisters Martha and Ana Albir. The Albir sisters inherited from their father the passion and knowledge to grow their own coffee. Later they acquired a mill and by 2016 they were providing milling and export services to neighboring small coffee farms. This new venture allowed them to contact directly with farms with socially conscious practices and they were also able to focus in separating of coffee by variety.',
@@ -113,6 +115,7 @@ Product.create!(
   name:'FUDAM',
   region:'La Uniõn, Nariño',
   process:'washed',
+  grind_options: ['Whole Beans','Ground for filter','Ground for Espresso','Ground for Aeropress'],
   cupping_notes:'tropical fruits, tamarind',
   description: "FUDAM stands for Fundación Agraria y Ambiental Para el Desarrollo Sostentible - a 300 member’s association of organic and Rainforest Alliance certified coffee growers founded in the year 2000.
   This group of smallholders lives in and around the municipality of La Unión in Nariño. Villages are at such high altitude that the growing areas, instead of being in higher grounds, lay bellow, surrounded by beautiful high peaks and rough roads.
@@ -127,6 +130,7 @@ Product.create!(
   name:'FUDAM',
   region:'La Uniõn, Nariño',
   process:'washed',
+  grind_options: ['Whole Beans','Ground for filter','Ground for Espresso','Ground for Aeropress'],
   cupping_notes:'tropical fruits, tamarind',
   description: "FUDAM stands for Fundación Agraria y Ambiental Para el Desarrollo Sostentible - a 300 member’s association of organic and Rainforest Alliance certified coffee growers founded in the year 2000.
   This group of smallholders lives in and around the municipality of La Unión in Nariño. Villages are at such high altitude that the growing areas, instead of being in higher grounds, lay bellow, surrounded by beautiful high peaks and rough roads.
@@ -141,6 +145,7 @@ Product.create!(
   name:'Tega & Tula farm',
   region:'Bonga, gifo, Keffa',
   process:'washed',
+  grind_options: ['Whole Beans','Ground for filter','Ground for Aeropress'],
   cupping_notes: 'pear, floral',
   description:'Tega & Tula speciality Coffee Farm located at Keffa province bio-reserve, takes its name after the two nearby villages in Woreda, Gibo district, Keffa, Ethiopia. The farm spreads out through 500 hectares where 400 hectares are planted with different coffee varieties. Ethiopian varieties, cultivars from the 70s as well as wild coffee from the Keffa forest, together all contribute for the uniqueness of the coffee grown here.
 Coffee harvest and de-pulping always takes place on the same day. The coffee beans are then fermented underwater for 36 hours. Is this anaerobic fermentation that enhances the flavors and aromas of each and every bean. It is then washed in local canals and thereafter, spends between 16 and18 hours soaking in a tank before being spread on the drying tables. It takes washed coffee beans between 7 to 11 days to dry out.',
@@ -154,6 +159,7 @@ Product.create!(
   name:'Tega & Tula farm',
   region:'Bonga, gifo, Keffa',
   process:'washed',
+  grind_options: ['Whole Beans','Ground for filter','Ground for Aeropress'],
   cupping_notes: 'pear, floral',
   description:'Tega & Tula speciality Coffee Farm located at Keffa province bio-reserve, takes its name after the two nearby villages in Woreda, Gibo district, Keffa, Ethiopia. The farm spreads out through 500 hectares where 400 hectares are planted with different coffee varieties. Ethiopian varieties, cultivars from the 70s as well as wild coffee from the Keffa forest, together all contribute for the uniqueness of the coffee grown here.
 Coffee harvest and de-pulping always takes place on the same day. The coffee beans are then fermented underwater for 36 hours. Is this anaerobic fermentation that enhances the flavors and aromas of each and every bean. It is then washed in local canals and thereafter, spends between 16 and18 hours soaking in a tank before being spread on the drying tables. It takes washed coffee beans between 7 to 11 days to dry out.',
@@ -167,6 +173,7 @@ Product.create!(
   name:'AMACA',
   region:'El Tambo, Cauca',
   process: 'washed',
+  grind_options: ['Whole Beans','Ground for filter','Ground for Espresso','Ground for Aeropress'],
   cupping_notes:'caramel, apple',
   description:'AMACA stands for Asociación de Mujeres Productoras Agropecuarias del Cauca, a group of 80 women’ producers who are based in El Tambo, Cauca, Colombia since 1999. This association has registered a steady growth and, today, it counts up to 140 women from three different villages in the same region. All of these women own the land they farm and are the heads of their households.
   The average farm size is one hectare per member, with many even smaller. Most members harvest only fully ripe cherries which are de-pulped on the same day of harvest and are then processed on their farms. Drying is done on raised beds inside parabolic dryers.',
@@ -180,6 +187,7 @@ Product.create!(
   name:'AMACA',
   region:'El Tambo, Cauca',
   process: 'washed',
+  grind_options: ['Whole Beans','Ground for filter','Ground for Espresso','Ground for Aeropress'],
   cupping_notes:'caramel, apple',
   description:'AMACA stands for Asociación de Mujeres Productoras Agropecuarias del Cauca, a group of 80 women’ producers who are based in El Tambo, Cauca, Colombia since 1999. This association has registered a steady growth and, today, it counts up to 140 women from three different villages in the same region. All of these women own the land they farm and are the heads of their households.
   The average farm size is one hectare per member, with many even smaller. Most members harvest only fully ripe cherries which are de-pulped on the same day of harvest and are then processed on their farms. Drying is done on raised beds inside parabolic dryers.',
