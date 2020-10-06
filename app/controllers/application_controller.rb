84857289c1acaa17989b0b8b836c2e9f22ab33d3
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-10-06 18:33:20 +0200

    hard translations to pt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :authenticate_user!
   before_action :set_order
   after_action :store_action
@@ -7,6 +8,15 @@ class ApplicationController < ActionController::Base
   # def default_url_options
   #   { host: ENV["DOMAIN"] || "localhost:3000" }
   # end
+
+  def set_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+
  def store_action
     return unless request.get?
     if (request.path != "/users/sign_in" &&
--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,26 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => 'users/registrations'}
 
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :show]
-  resources :pages, only: [:home, :new, :create, :about, :brewing]
-  # get :coffee_product, controller:'products'
-  get :coffee_product, to: "products#coffee_product"
+  scope '(:locale)', locale: /fr|pt/ do
+    root to: 'pages#home'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :products, only: [:index, :show]
+    resources :pages, only: [:home, :new, :create, :about, :brewing]
+    # get :coffee_product, controller:'products'
+    get :coffee_product, to: "products#coffee_product"
 
-  get :about, to: "pages#about"
-  get :brewing, to: "pages#brewing"
-  get :contact, to: 'pages#home'
+    get :about, to: "pages#about"
+    get :brewing, to: "pages#brewing"
+    get :contact, to: 'pages#home'
 
 
- resources :order_items
+   resources :order_items
 
- resources :orders, only: [:show, :update] do
-    resources :payments, only: :new
-    resources :messages, only: :new
- end
+   resources :orders, only: [:show, :update] do
+      resources :payments, only: :new
+      resources :messages, only: :new
+   end
+end
 
  mount StripeEvent::Engine, at: '/stripe-webhooks'
 
