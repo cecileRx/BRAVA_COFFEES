3242ee233b6a22d6c8262130405cf998d464cd58
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-10-25 20:44:40 +0100

    translation views first part

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,57 +17,57 @@ equipment = Category.create!(name: 'equipment')
 puts 'Creating equipments...'
 
 Product.create!(
-  name: I18n.t('chemex_3_name'),
-  description: I18n.t('chemex_desc'),
+  name: 'chemex_3_name',
+  description: 'chemex_desc',
   photo: 'chemex_3_cups.jpg',
   price_cents: 4369,
   category: equipment)
 
 Product.create!(
-  name: I18n.t('chemex_6_name'),
-  description: I18n.t('chemex_desc'),
+  name: 'chemex_6_name',
+  description: 'chemex_desc',
   photo: 'chemex_6_cups.jpg',
   price_cents: 4949,
   category: equipment)
 
 Product.create!(
-  name: I18n.t('V60_name'),
-  description: I18n.t('kit_v_60_desc'),
+  name: 'V60_name',
+  description: 'kit_v_60_desc',
   photo: 'Kit_V60.jpg',
   price_cents: 3999,
   category: equipment)
 
 Product.create!(
-  name: I18n.t('chemex_3_filter_name'),
-  description: I18n.t('chemex_3_filter_desc'),
+  name: 'chemex_3_filter_name',
+  description: 'chemex_3_filter_desc',
   photo: 'Filtro_Chemex_3_cups.jpg',
   price_cents: 1239,
   category: equipment)
 
 Product.create!(
-  name: I18n.t('chemex_6_filter_name'),
-  description: I18n.t('chemex_6_filter_desc'),
+  name: 'chemex_6_filter_name',
+  description: 'chemex_6_filter_desc',
   photo: 'Filtro_Chemex_6_cups.jpg',
   price_cents: 1239,
   category: equipment)
 
 Product.create!(
-  name: I18n.t('hario_01_name'),
-  description: I18n.t('hario_01_desc'),
+  name: 'hario_01_name',
+  description: 'hario_01_desc',
   photo: 'Hario_V60_Filters.jpg',
   price_cents: 329,
   category: equipment)
 
 Product.create!(
-  name: I18n.t('hario_02_name'),
+  name: 'hario_02_name',
   photo: 'Hario_V60_Filters.jpg',
-  description: I18n.t('hario_02_desc'),
+  description: 'hario_02_desc',
   price_cents: 349,
   category: equipment)
 
 Product.create!(
-  name: I18n.t('Mesh_dripper_name'),
-  description: I18n.t('mesh_dripper_desc'),
+  name: 'Mesh_dripper_name',
+  description: 'mesh_dripper_desc',
   photo: 'Mesh_Metal_Dripper_2_cups.jpg',
   price_cents: 3289,
   category: equipment)
@@ -77,102 +77,104 @@ puts 'Creating coffees...'
 Product.create!(
   weight: 250,
   price_cents: 1250,
-  origin: I18n.t('nica_origin'),
+  origin: 'nica_origin',
   name:'Cerro de Jesus farm',
   region: 'Jalapa, Neuva Segovia',
-  process:  I18n.t('process_att'),
-  grind_options: I18n.t('nica_grind'),
-  cupping_notes: I18n.t('nica_cp'),
-  description: I18n.t('nica_desc'),
-   category: coffee)
+  process:  'process_att',
+  grind_options: 'nica_grind',
+  cupping_notes: 'nica_cp',
+  description: 'nica_desc',
+  photo: 'nicaragua.jpg',
+  category: coffee)
 
 Product.create!(
   weight: 1000,
   price_cents: 4366,
-  origin: I18n.t('nica_origin'),
+  origin: 'nica_origin',
   name:'Cerro de Jesus farm',
   region: 'Jalapa, Neuva Segovia',
-  process:  I18n.t('process_att'),
-  grind_options: I18n.t('nica_grind'),
-  cupping_notes: I18n.t('nica_cp'),
-  description: I18n.t('nica_desc'),
-   category: coffee)
+  process:  'process_att',
+  grind_options: 'nica_grind',
+  cupping_notes: 'nica_cp',
+  description: 'nica_desc',
+  photo: 'nicaragua.jpg',
+  category: coffee)
 
 Product.create!(
   weight: 250,
   price_cents: 900,
-  origin: I18n.t('columb_fudam_origin'),
+  origin: 'columb_fudam_origin',
   name:'FUDAM',
   region:'La Uniõn, Nariño',
-  process: I18n.t('process_att'),
-  grind_options: I18n.t('columb_fudam_grind'),
-  cupping_notes:I18n.t('columb_fudam_cp'),
-  description: I18n.t('columb_fudam_desc'),
+  process: 'process_att',
+  grind_options: 'columb_fudam_grind',
+  cupping_notes:'columb_fudam_cp',
+  description: 'columb_fudam_desc',
   photo: 'columbia_fundam.jpg',
   category: coffee)
 
 Product.create!(
   weight: 1000,
   price_cents: 2960,
-  origin: I18n.t('columb_fudam_origin'),
+  origin: 'columb_fudam_origin',
   name:'FUDAM',
   region:'La Uniõn, Nariño',
-  process: I18n.t('process_att'),
-  grind_options: I18n.t('columb_fudam_grind'),
-  cupping_notes: I18n.t('columb_fudam_cp'),
-  description: I18n.t('columb_fudam_desc'),
+  process: 'process_att',
+  grind_options: 'columb_fudam_grind',
+  cupping_notes: 'columb_fudam_cp',
+  description: 'columb_fudam_desc',
   photo: 'columbia_fundam.jpg',
   category: coffee)
 
 Product.create!(
   weight: 250,
   price_cents: 1130,
-  origin: I18n.t('ethiopia_origin'),
+  origin: 'ethiopia_origin',
   name:'Tega & Tula farm',
   region:'Bonga, gifo, Keffa',
-  process: I18n.t('process_att'),
-  grind_options: I18n.t('ethiopia_grind'),
-  cupping_notes: I18n.t('ethiopia_cp'),
-  description: I18n.t('ethiopia_desc'),
+  process: 'process_att',
+  grind_options: 'ethiopia_grind',
+  cupping_notes: 'ethiopia_cp',
+  description: 'ethiopia_desc',
 photo:'ethiopia.jpg',
 category: coffee)
 
 Product.create!(
   price_cents: 3880,
   weight: 1000,
-  origin: I18n.t('ethiopia_origin'),
+  origin: 'ethiopia_origin',
   name:'Tega & Tula farm',
   region:'Bonga, gifo, Keffa',
-  process: I18n.t('process_att'),
-  grind_options: I18n.t('ethiopia_grind'),
-  cupping_notes: I18n.t('ethiopia_cp'),
-  description: I18n.t('ethiopia_desc'),
+  process: 'process_att',
+  grind_options: 'ethiopia_grind',
+  cupping_notes: 'ethiopia_cp',
+  description: 'ethiopia_desc',
 photo:'ethiopia.jpg',
 category: coffee)
 
 Product.create!(
   price_cents: 1041,
   weight: 250,
-  origin: I18n.t('columb_amaca_origin'),
+  origin: 'columb_amaca_origin',
   name:'AMACA',
   region:'El Tambo, Cauca',
-  process:  I18n.t('process_att'),
-  grind_options: I18n.t('columb_amaca_grind'),
-  cupping_notes: I18n.t('columb_amaca_cp'),
-  description: I18n.t('columb_amaca_desc'),
+  process:  'process_att',
+  grind_options: 'columb_amaca_grind',
+  cupping_notes: 'columb_amaca_cp',
+  description: 'columb_amaca_desc',
   photo: 'columbia_amaca2.jpg',
   category: coffee)
 
 Product.create!(
   price_cents: 3524,
   weight: 1000,
-  origin: I18n.t('columb_amaca_origin'),
+  origin: 'columb_amaca_origin',
   name:'AMACA',
   region:'El Tambo, Cauca',
-  process:  I18n.t('process_att'),
-  grind_options: I18n.t('columb_amaca_grind'),
-  cupping_notes: I18n.t('columb_amaca_cp'),
-  description: I18n.t('columb_amaca_desc'),
+  process:  'process_att',
+  grind_options: 'columb_amaca_grind',
+  cupping_notes: 'columb_amaca_cp',
+  description: 'columb_amaca_desc',
   photo: 'columbia_amaca2.jpg',
   category: coffee)
 
