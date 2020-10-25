7140b51e69e0232de64c57287ed743e3c68bf32e
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-10-25 11:11:57 +0100

    seeds translation

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,62 +17,57 @@ equipment = Category.create!(name: 'equipment')
 puts 'Creating equipments...'
 
 Product.create!(
-  name: 'Chemex 3 cups',
-  description: 'CHEMEX delivers the purest flavor experience. Made of non-porous Borosilicate glass which will not absorb odors or chemical residues.
-  CHEMEX pour-over allows coffee to be covered and refrigerated for reheating without losing flavor
-  Simple, easy to use with timeless, elegant design',
+  name: I18n.t('chemex_3_name'),
+  description: I18n.t('chemex_desc'),
   photo: 'chemex_3_cups.jpg',
   price_cents: 4369,
   category: equipment)
 
 Product.create!(
-  name: 'Chemex 6 cups',
-  description: 'CHEMEX delivers the purest flavor experience. Made of non-porous Borosilicate glass which will not absorb odors or chemical residues
-  CHEMEX pour-over allows coffee to be covered and refrigerated for reheating without losing flavor
-  Simple, easy to use with timeless, elegant design',
+  name: I18n.t('chemex_6_name'),
+  description: I18n.t('chemex_desc'),
   photo: 'chemex_6_cups.jpg',
   price_cents: 4949,
   category: equipment)
 
 Product.create!(
-  name: 'Kit V60',
-  description: 'This particular coffee dripper is made of copper - a particularly high-end dripper, but they come in ceramic for maximum heat retention, cost-effective plastic, and heat-proof glass. Also, you can choose from three different sizes 01 / 02 and 03, depending on your needs.',
+  name: I18n.t('V60_name'),
+  description: I18n.t('kit_v_60_desc'),
   photo: 'Kit_V60.jpg',
   price_cents: 3999,
   category: equipment)
 
 Product.create!(
-  name: 'Filtro Chemex 3 cups',
-  description: 'Paper filters for chemex 3 cups',
+  name: I18n.t('chemex_3_filter_name'),
+  description: I18n.t('chemex_3_filter_desc'),
   photo: 'Filtro_Chemex_3_cups.jpg',
   price_cents: 1239,
   category: equipment)
 
 Product.create!(
-  name: 'Filtro Chemex 6 cups',
-  description: 'Paper filters for chemex 6 cups',
+  name: I18n.t('chemex_6_filter_name'),
+  description: I18n.t('chemex_6_filter_desc'),
   photo: 'Filtro_Chemex_6_cups.jpg',
   price_cents: 1239,
   category: equipment)
 
 Product.create!(
-  name: 'Hario V60 Coffee paper filters 01',
-  description: 'paper filters for V60 - 01 to 02 cups size',
+  name: I18n.t('hario_01_name'),
+  description: I18n.t('hario_01_desc'),
   photo: 'Hario_V60_Filters.jpg',
   price_cents: 329,
   category: equipment)
 
 Product.create!(
-  name: 'Hario V60 Coffee paper filters 02',
+  name: I18n.t('hario_02_name'),
   photo: 'Hario_V60_Filters.jpg',
-  description: 'paper filters for V60 - 01 to 04 cups size',
+  description: I18n.t('hario_02_desc'),
   price_cents: 349,
   category: equipment)
 
 Product.create!(
-  name: 'Double Mesh Metal Dripper 1_2 cups',
-  description: 'Dripper with double mesh filters. It is made solely of stainless steel.
-    The metal filters allow the coffee oil to slowly pour through, letting you enjoy coffee with a deep aroma.',
+  name: I18n.t('Mesh_dripper_name'),
+  description: I18n.t('mesh_dripper_desc'),
   photo: 'Mesh_Metal_Dripper_2_cups.jpg',
   price_cents: 3289,
   category: equipment)
@@ -82,114 +77,102 @@ puts 'Creating coffees...'
 Product.create!(
   weight: 250,
   price_cents: 1250,
-  origin: 'Nicaragua',
+  origin: I18n.t('nica_origin'),
   name:'Cerro de Jesus farm',
   region: 'Jalapa, Neuva Segovia',
-  process:  I18n.t('process'),
-  grind_options: ['Whole Beans','Filter','Aeropress'],
+  process:  I18n.t('process_att'),
+  grind_options: I18n.t('nica_grind'),
   cupping_notes: I18n.t('nica_cp'),
-  description: 'Cerro de Jesus or Jesus Mountain Farm is located in Jalapa, Neuva Segovia, on the second highest peak of Nicaragua. The farm spreads through 700 hectares of which about 400 hectares are a beautiful forest reserve.
-  In 2017, Jesus Mountain Farm decided to partner with Cafetos de Segovia, a small specialty mill and export company run by two sisters Martha and Ana Albir. The Albir sisters inherited from their father the passion and knowledge to grow their own coffee. Later they acquired a mill and by 2016 they were providing milling and export services to neighboring small coffee farms. This new venture allowed them to contact directly with farms with socially conscious practices and they were also able to focus in separating of coffee by variety.',
-   photo:'nicaragua.jpg',
+  description: I18n.t('nica_desc'),
    category: coffee)
 
 Product.create!(
   weight: 1000,
   price_cents: 4366,
-  origin: 'Nicaragua',
+  origin: I18n.t('nica_origin'),
   name:'Cerro de Jesus farm',
   region: 'Jalapa, Neuva Segovia',
-  process:  I18n.t('process'),
-  grind_options: ['Whole Beans','Filter','Aeropress'],
+  process:  I18n.t('process_att'),
+  grind_options: I18n.t('nica_grind'),
   cupping_notes: I18n.t('nica_cp'),
-  description: 'Cerro de Jesus or Jesus Mountain Farm is located in Jalapa, Neuva Segovia, on the second highest peak of Nicaragua. The farm spreads through 700 hectares of which about 400 hectares are a beautiful forest reserve.
-  In 2017, Jesus Mountain Farm decided to partner with Cafetos de Segovia, a small specialty mill and export company run by two sisters Martha and Ana Albir. The Albir sisters inherited from their father the passion and knowledge to grow their own coffee. Later they acquired a mill and by 2016 they were providing milling and export services to neighboring small coffee farms. This new venture allowed them to contact directly with farms with socially conscious practices and they were also able to focus in separating of coffee by variety.',
-   photo:'nicaragua.jpg',
+  description: I18n.t('nica_desc'),
    category: coffee)
 
 Product.create!(
   weight: 250,
   price_cents: 900,
-  origin: 'Colombia',
+  origin: I18n.t('columb_fudam_origin'),
   name:'FUDAM',
   region:'La Uniõn, Nariño',
-  process: I18n.t('process'),
-  grind_options: ['Whole Beans','Ground for filter','Ground for Espresso','Ground for Aeropress'],
+  process: I18n.t('process_att'),
+  grind_options: I18n.t('columb_fudam_grind'),
   cupping_notes:I18n.t('columb_fudam_cp'),
-  description: "FUDAM stands for Fundación Agraria y Ambiental Para el Desarrollo Sostentible - a 300 member’s association of organic and Rainforest Alliance certified coffee growers founded in the year 2000.
-  This group of smallholders lives in and around the municipality of La Unión in Nariño. Villages are at such high altitude that the growing areas, instead of being in higher grounds, lay bellow, surrounded by beautiful high peaks and rough roads.
-  Today, FUDAM's growers still firmly believe in the principles of sustainability, the ideal that brought them together. Recently, when asked why they continue to farm organically, despite the pressure to rely on chemical inputs, the association explained: 'This is just how we live; these are our values and our way of life.'",
+  description: I18n.t('columb_fudam_desc'),
   photo: 'columbia_fundam.jpg',
   category: coffee)
 
 Product.create!(
   weight: 1000,
   price_cents: 2960,
-  origin: 'Colombia',
+  origin: I18n.t('columb_fudam_origin'),
   name:'FUDAM',
   region:'La Uniõn, Nariño',
-  process: I18n.t('process'),
-  grind_options: ['Whole Beans','Ground for filter','Ground for Espresso','Ground for Aeropress'],
-  cupping_notes:I18n.t('columb_fudam_cp'),
-  description: "FUDAM stands for Fundación Agraria y Ambiental Para el Desarrollo Sostentible - a 300 member’s association of organic and Rainforest Alliance certified coffee growers founded in the year 2000.
-  This group of smallholders lives in and around the municipality of La Unión in Nariño. Villages are at such high altitude that the growing areas, instead of being in higher grounds, lay bellow, surrounded by beautiful high peaks and rough roads.
-  Today, FUDAM's growers still firmly believe in the principles of sustainability, the ideal that brought them together. Recently, when asked why they continue to farm organically, despite the pressure to rely on chemical inputs, the association explained: 'This is just how we live; these are our values and our way of life.'",
+  process: I18n.t('process_att'),
+  grind_options: I18n.t('columb_fudam_grind'),
+  cupping_notes: I18n.t('columb_fudam_cp'),
+  description: I18n.t('columb_fudam_desc'),
   photo: 'columbia_fundam.jpg',
   category: coffee)
 
 Product.create!(
   weight: 250,
   price_cents: 1130,
-  origin: 'Ethiopia',
+  origin: I18n.t('ethiopia_origin'),
   name:'Tega & Tula farm',
   region:'Bonga, gifo, Keffa',
-  process: I18n.t('process'),
-  grind_options: ['Whole Beans','Ground for filter','Ground for Aeropress'],
-  cupping_notes: 'pear, floral',
-  description:'Tega & Tula speciality Coffee Farm located at Keffa province bio-reserve, takes its name after the two nearby villages in Woreda, Gibo district, Keffa, Ethiopia. The farm spreads out through 500 hectares where 400 hectares are planted with different coffee varieties. Ethiopian varieties, cultivars from the 70s as well as wild coffee from the Keffa forest, together all contribute for the uniqueness of the coffee grown here.
-Coffee harvest and de-pulping always takes place on the same day. The coffee beans are then fermented underwater for 36 hours. Is this anaerobic fermentation that enhances the flavors and aromas of each and every bean. It is then washed in local canals and thereafter, spends between 16 and18 hours soaking in a tank before being spread on the drying tables. It takes washed coffee beans between 7 to 11 days to dry out.',
+  process: I18n.t('process_att'),
+  grind_options: I18n.t('ethiopia_grind'),
+  cupping_notes: I18n.t('ethiopia_cp'),
+  description: I18n.t('ethiopia_desc'),
 photo:'ethiopia.jpg',
 category: coffee)
 
 Product.create!(
   price_cents: 3880,
   weight: 1000,
-  origin: 'Ethiopia',
+  origin: I18n.t('ethiopia_origin'),
   name:'Tega & Tula farm',
   region:'Bonga, gifo, Keffa',
-  process: I18n.t('process'),
-  grind_options: ['Whole Beans','Ground for filter','Ground for Aeropress'],
-  cupping_notes: 'pear, floral',
-  description:'Tega & Tula speciality Coffee Farm located at Keffa province bio-reserve, takes its name after the two nearby villages in Woreda, Gibo district, Keffa, Ethiopia. The farm spreads out through 500 hectares where 400 hectares are planted with different coffee varieties. Ethiopian varieties, cultivars from the 70s as well as wild coffee from the Keffa forest, together all contribute for the uniqueness of the coffee grown here.
-Coffee harvest and de-pulping always takes place on the same day. The coffee beans are then fermented underwater for 36 hours. Is this anaerobic fermentation that enhances the flavors and aromas of each and every bean. It is then washed in local canals and thereafter, spends between 16 and18 hours soaking in a tank before being spread on the drying tables. It takes washed coffee beans between 7 to 11 days to dry out.',
+  process: I18n.t('process_att'),
+  grind_options: I18n.t('ethiopia_grind'),
+  cupping_notes: I18n.t('ethiopia_cp'),
+  description: I18n.t('ethiopia_desc'),
 photo:'ethiopia.jpg',
 category: coffee)
 
 Product.create!(
   price_cents: 1041,
   weight: 250,
-  origin: 'Colombia',
+  origin: I18n.t('columb_amaca_origin'),
   name:'AMACA',
   region:'El Tambo, Cauca',
-  process:  I18n.t('process'),
-  grind_options: ['Whole Beans','Ground for filter','Ground for Espresso','Ground for Aeropress'],
-  cupping_notes:'caramel, apple',
-  description:'AMACA stands for Asociación de Mujeres Productoras Agropecuarias del Cauca, a group of 80 women’ producers who are based in El Tambo, Cauca, Colombia since 1999. This association has registered a steady growth and, today, it counts up to 140 women from three different villages in the same region. All of these women own the land they farm and are the heads of their households.
-  The average farm size is one hectare per member, with many even smaller. Most members harvest only fully ripe cherries which are de-pulped on the same day of harvest and are then processed on their farms. Drying is done on raised beds inside parabolic dryers.',
+  process:  I18n.t('process_att'),
+  grind_options: I18n.t('columb_amaca_grind'),
+  cupping_notes: I18n.t('columb_amaca_cp'),
+  description: I18n.t('columb_amaca_desc'),
   photo: 'columbia_amaca2.jpg',
   category: coffee)
 
 Product.create!(
   price_cents: 3524,
   weight: 1000,
-  origin: 'Colombia',
+  origin: I18n.t('columb_amaca_origin'),
   name:'AMACA',
   region:'El Tambo, Cauca',
-  process:  I18n.t('process'),
-  grind_options: ['Whole Beans','Ground for filter','Ground for Espresso','Ground for Aeropress'],
-  cupping_notes:'caramel, apple',
-  description:'AMACA stands for Asociación de Mujeres Productoras Agropecuarias del Cauca, a group of 80 women’ producers who are based in El Tambo, Cauca, Colombia since 1999. This association has registered a steady growth and, today, it counts up to 140 women from three different villages in the same region. All of these women own the land they farm and are the heads of their households.
-  The average farm size is one hectare per member, with many even smaller. Most members harvest only fully ripe cherries which are de-pulped on the same day of harvest and are then processed on their farms. Drying is done on raised beds inside parabolic dryers.',
+  process:  I18n.t('process_att'),
+  grind_options: I18n.t('columb_amaca_grind'),
+  cupping_notes: I18n.t('columb_amaca_cp'),
+  description: I18n.t('columb_amaca_desc'),
   photo: 'columbia_amaca2.jpg',
   category: coffee)
 
