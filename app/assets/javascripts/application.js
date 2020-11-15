dec61b590847f50fa0cf55e955bd9636d8604b4e
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-11-15 18:32:28 +0100

    light ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,16 @@
 //= require jquery
 
 
+
+
 function displayPrice() {
   console.log('hello')
   const selectedWeight = document.querySelector("#selected_weight");
   let sel_weight_val = selectedWeight.value;
   console.log(sel_weight_val);
-  const selectedQuantity = document.querySelector("#selected_quantity");
-  let sel_quantity_val = selectedQuantity.value;
-  console.log(sel_quantity_val);
+  // const selectedQuantity = document.querySelector("#selected_quantity");
+  // let sel_quantity_val = selectedQuantity.value;
+
 
   const totalPrice = document.querySelector("#display_price");
 
@@ -35,11 +37,11 @@ function displayPrice() {
     //convert to float number with . and not with ,
       const maxWeightPrice = parseFloat(document.getElementById("hidden_price_max").textContent.replace(",","."));
 
-      totalPrice.innerHTML = `${maxWeightPrice * sel_quantity_val }€`;
+      totalPrice.innerHTML = `${maxWeightPrice}€`;
     } else {
       const minWeightPrice = parseFloat(document.getElementById("hidden_price_min").textContent.replace(",","."));
 
-      totalPrice.innerHTML = `${minWeightPrice * sel_quantity_val}€`;
+      totalPrice.innerHTML = `${minWeightPrice}€`;
     }
 
 }
