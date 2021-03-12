dea633154e5c49004b3a9ca5fd2b74a7cc88e58d
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2021-03-12 14:31:14 +0000

    automatic mails ok

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,12 @@
 class OrderMailer < ApplicationMailer
   def new_order_email
     # @order = Order.find(params[:order_id])
+
     @order = params[:order]
     @user = User.find_by(id: @order.user_id)
-
+    attachments.inline["Brava_Logo_mail.png"] = File.read("#{Rails.root}/app/assets/images/Brava_Logo_mail.png")
+    attachments.inline["facebook-square-brands.png"] = File.read("#{Rails.root}/app/assets/images/facebook-square-brands.png")
+     attachments.inline["instagram-square-brands.png"] = File.read("#{Rails.root}/app/assets/images/instagram-square-brands.png")
     mail(to: "#{@user.email}", subject: t(".mail_subject"))
   end
 end
